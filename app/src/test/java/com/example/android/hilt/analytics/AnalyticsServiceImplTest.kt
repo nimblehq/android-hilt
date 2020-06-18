7d0f2fdb1366becf85c6b0b97f0fb32562f1ba43
trung <cs.leetrung@gmail.com>
2020-06-18 13:57:08 +0700

    Minor refactor and use Hilt @Inject context for unit testing

--- app/src/test/java/com/example/android/hilt/analytics/AnalyticsServiceImplTest.kt
+++ app/src/test/java/com/example/android/hilt/analytics/AnalyticsServiceImplTest.kt
@@ -1,17 +1,17 @@
 package com.example.android.hilt.analytics
 
 import android.content.Context
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.hilt.R
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.testing.*
 import org.amshove.kluent.shouldBe
 import org.amshove.kluent.shouldNotBe
-import org.junit.Rule
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 import org.robolectric.shadows.ShadowToast
+import javax.inject.Inject
 
 @RunWith(AndroidJUnit4::class)
 @HiltAndroidTest
@@ -20,20 +20,27 @@ class AnalyticsServiceImplTest {
 
     @get:Rule var hiltRule = HiltAndroidRule(this)
 
-    private val appContext = ApplicationProvider.getApplicationContext<Context>()
+    @Inject
+    @ApplicationContext lateinit var context: Context
 
-    @Test
-    fun `Validate real AnalyticsServiceImpl, it tracks correctly on landing`() {
-        val analyticsServiceImpl = AnalyticsServiceImpl(appContext)
-        analyticsServiceImpl.track("Hello")
-
-        val expectedValue = appContext.resources.getString(R.string.analytics_notification, "Hello")
-        ShadowToast.showedToast(expectedValue) shouldBe true
+    @Before
+    fun setup() {
+        // Now the above @Inject fields can be used for our test cases
+        hiltRule.inject()
     }
 
     @Test
-    fun `Validate getter`() {
-        val analyticsServiceImpl = AnalyticsServiceImpl(appContext)
+    fun `Validate injected components are available`() {
+        val analyticsServiceImpl = AnalyticsServiceImpl(context)
         analyticsServiceImpl.context shouldNotBe null
+    }
+
+    @Test
+    fun `Validate real AnalyticsServiceImpl, it tracks correctly on landing`() {
+        val analyticsServiceImpl = AnalyticsServiceImpl(context)
+        analyticsServiceImpl.track("Hello")
+
+        val expectedValue = context.resources.getString(R.string.analytics_notification, "Hello")
+        ShadowToast.showedToast(expectedValue) shouldBe true
     }
 }
